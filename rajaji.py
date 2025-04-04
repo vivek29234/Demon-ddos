2a215d140fb39f335522f5f5e11e86795925e743
vivek29234 <mvkm665@gmail.com>
2025-04-04 19:05:19 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7001693054:AAG-F5iAyaEJBjl2_3PuzRfLDBiHqyuAWLM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@WerewolfDemon")
+OWNER_USERNAME=("WerewolfDemon")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("6656608288")
