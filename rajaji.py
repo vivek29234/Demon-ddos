faf89dec35101ddd4d65afea6f91802d9867b46c
vivek29234 <mvkm665@gmail.com>
2025-04-04 18:39:01 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7001693054:AAG-F5iAyaEJBjl2_3PuzRfLDBiHqyuAWLM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@WerewolfDemon")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6656608288")
+
+
+
+
+
+  
